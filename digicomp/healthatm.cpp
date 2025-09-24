351deed82b2a3b2046eb3422e4dc13160ccb67b3
hamzahtaro6 <hamzahbk12345@gmail.com>
2025-09-24 20:28:18 +0800

    oke

--- digicomp/healthatm.cpp
+++ digicomp/healthatm.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
+    int bloodPressure;
+    int sugarLevel;
+
     cout << "=== AI Health ATM ===" << endl;
+    cout << "Enter your blood pressure (systolic): ";
+    cin >> bloodPressure;
+
+    cout << "Enter your blood sugar level (mg/dL): ";
+    cin >> sugarLevel;
+
     return 0;
 }
